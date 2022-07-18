92baebcf9ba85a38af95da53f64d14052eea3f66
thsaudwls <39595042+thsaudwls@users.noreply.github.com>
2022-07-18 13:03:13 +0900

    Add files via upload

---
+++ libft/ft_striteri.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnyeong._.jin <rnyeong._.jin@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/12 17:33:24 by myson             #+#    #+#             */
+/*   Updated: 2022/05/03 02:13:26 by rnyeong._.j      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	len;
+	unsigned int	i;
+
+	i = 0;
+	if (s == 0x00)
+		return ;
+	len = ft_strlen(s);
+	while (i < len)
+	{
+		f(i, &s[i]);
+		i ++;
+	}
+}
